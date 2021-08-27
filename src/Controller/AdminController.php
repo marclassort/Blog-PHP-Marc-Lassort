698a9410bc2472f785043640150c03c88e5d535c
marclassort <marc.lassort@gmail.com>
2021-08-27 15:57:07 +0200

    CRUD of posts - Correcting remaining bugs

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -14,27 +14,20 @@ class AdminController extends BaseController
 
     public function admin() 
     {
-        $string = "Blog pro de Marc Lassort";
 
-        return $this->render('backend/admin.html.twig', [
-            "string" => $string
-        ]);
+        return $this->render('backend/admin.html.twig', []);
     }
 
     public function createPost()
     {
         $post = new Post($_POST);
-        // $image = new Image($_POST);
 
         if (!empty($_POST))
         {
             $postManager = new PostManager('post', 'Post');
             $postManager->createPost($post);
 
-            // $imageManager = new ImageManager('image', 'Image');
-            // $imageManager->setImage($image, 1);
-
-            return $this->redirect('liste-articles');
+            $this->redirect('liste-articles');
         }
         
         return $this->render('backend/postForm.html.twig', []);
@@ -42,8 +35,6 @@ class AdminController extends BaseController
 
     public function listPosts()
     {
-        $string = "Blog pro de Marc Lassort";
-
         $postManager = new PostManager('post', 'Post');
         $imageManager = new ImageManager('image', 'Image');
 
@@ -51,7 +42,6 @@ class AdminController extends BaseController
         $images = $imageManager->getImages();
 
         return $this->render('backend/postList.html.twig', [
-            "string" => $string,
             "posts" => $posts,
             "images" => $images
         ]);
@@ -59,28 +49,15 @@ class AdminController extends BaseController
 
     public function editPost($idPost)
     {
-        // récupérer en BDD le post à modifier (stocker dans la variable $post et le passer au render)
         $postManager = new PostManager('post', 'Post');
         $post = $postManager->getPost($idPost);
         $imageManager = new ImageManager('image', 'Image');
         $image = $imageManager->getImage($idPost);
 
-        // vérifier que le formulaire a été soumis et valide (méthodes de BaseController) -> if 
         if ($this->isSubmitted('editInput') && $this->isValid($post))
         {   
             $postManager->editPost($post);
-            // je set le contenu, le titre, etc. 
-            // $post->setTitle($idPost)
-            //     ->setBlurb()
-            //     ->setContent()
-            //     ->setAuthor();
-
-            // envoyer ces valeurs en BDD
-
-            
-            // Appeler sa méthode pour update en base de données et faire passer le $post en paramètre 
-            // return à la fin du if et redirect du BaseController vers le tableau des posts (? message flash)
-            return $this->redirect('liste-articles');
+            $this->redirect('liste-articles');
         }
 
         return $this->render('backend/postEdit.html.twig', [
@@ -94,19 +71,16 @@ class AdminController extends BaseController
         $postManager = new PostManager('post', 'Post');
         $postManager->deletePost($idPost);
 
-        return $this->redirect('liste-articles');
+        $this->redirect('liste-articles');
     }
 
     public function manageComments()
     {
-        $string = "Blog pro de Marc Lassort";
-
         $commentManager = new CommentManager('comment', 'Comment');
 
         $comments = $commentManager->getAllComments();
 
         return $this->render('backend/manageComments.html.twig', [
-            "string" => $string,
             "comments" => $comments
         ]);
     }
